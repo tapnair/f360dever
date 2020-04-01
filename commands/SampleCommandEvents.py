f5fdef8a3e43a1fc425d88a3324708ee54faeb5d
Patrick Rainsberry <patrick.rainsberry@autodesk.com>
2020-04-01 10:49:34 -0700

    ui commands

--- commands/SampleCommandEvents.py
+++ commands/SampleCommandEvents.py
@@ -9,13 +9,75 @@ def palette_push(cmd_id, palette_id):
     ao = apper.AppObjects()
     palette = ao.ui.palettes.itemById(palette_id)
     app = adsk.core.Application.get()
+    cmd = ao.ui.commandDefinitions.itemById(cmd_id)
+    toolbar_tab_msg = make_tab_message_list()
 
     action_data = {'cmd_id': cmd_id,
-                   'version': app.version
+                   'version': app.version,
+                   'toolbar_tab_msg': toolbar_tab_msg
                    }
     # Send message to the HTML Page
     if palette:
         palette.sendInfoToHTML('command', json.dumps(action_data))
+
+
+def make_tab_message():
+
+    msg = ""
+    ao = apper.AppObjects()
+
+    tab: adsk.core.ToolbarTab
+    for tab in ao.ui.activeWorkspace.toolbarTabs:
+
+        if tab.isActive:
+            msg += "Toolbar Tab: {}\n".format(tab.id)
+            toolbar_panel: adsk.core.ToolbarPanel
+            for toolbar_panel in tab.toolbarPanels:
+                msg += "    Panel: {}\n".format(toolbar_panel.id)
+                toolbar_control: adsk.core.ToolbarControl
+                for toolbar_control in toolbar_panel.controls:
+                    msg += "     - {}\n".format(toolbar_control.id)
+
+    return msg
+
+
+def make_tab_message_list():
+
+    msg = ""
+    ao = apper.AppObjects()
+
+    tab: adsk.core.ToolbarTab
+    for tab in ao.ui.activeWorkspace.toolbarTabs:
+
+        if tab.isActive:
+            msg += "Toolbar Tab: {}<OL>".format(tab.id)
+            toolbar_panel: adsk.core.ToolbarPanel
+            for toolbar_panel in tab.toolbarPanels:
+                msg += "<LI>Panel: {}<UL>\n".format(toolbar_panel.id)
+                toolbar_control: adsk.core.ToolbarControl
+                for toolbar_control in toolbar_panel.controls:
+                    msg += "<LI>{}</LI>".format(toolbar_control.id)
+                msg += "</UL></LI>"
+            msg += "</OL>"
+    return msg
+
+
+def make_panel_message_list():
+
+    msg = ""
+    ao = apper.AppObjects()
+
+    msg += "<OL>"
+    toolbar_panel: adsk.core.ToolbarPanel
+    for toolbar_panel in ao.ui.activeWorkspace.toolbarPanels:
+
+        msg += "<LI>Panel: {}<UL>\n".format(toolbar_panel.id)
+        toolbar_control: adsk.core.ToolbarControl
+        for toolbar_control in toolbar_panel.controls:
+            msg += "<LI>{}</LI>".format(toolbar_control.id)
+        msg += "</UL></LI>"
+    msg += "</OL>"
+    return msg
 
 
 class CommandStreamEvent(apper.Fusion360CommandEvent):
